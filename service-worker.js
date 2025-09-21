d31a5e48e38bd69e70c1e382eec8c2e4ff8f95eb
marcarvalho44 <marcarvalho29@gmail.com>
2025-09-21 10:41:32 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,2 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('nbc-blue-v1').then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])))});
+self.addEventListener('install',e=>{e.waitUntil(caches.open('nbc-creme-v1').then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
